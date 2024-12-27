db51cb43bd4293c0abb1a6e87613a46484afb753
Fabrizio Gortani <fabrizio.gortani@beliven.com>
2024-12-27 21:22:24 +0100

    feat: add install bash

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+trap _cleanup EXIT
+
+_cleanup() {
+    rm -rf /tmp/archive.zip
+    rm -rf /tmp/archive
+}
+
+echo "Download the latest relase..."
+
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/beliven-it/laravel-sync/releases/latest | grep "zipball_url" | cut -d '"' -f 4)
+
+echo "Latest release: $LATEST_RELEASE"
+
+if [ -n "$LATEST_RELEASE" ]; then
+    curl -sL "$LATEST_RELEASE" -o /tmp/archive.zip
+    unzip -q -j /tmp/archive.zip -d /tmp/archive
+    cp -r /tmp/archive/laravel-sync .
+    chmod +x laravel-sync
+    echo "Done!"
+else
+    echo "Cannot find the latest release"
+    exit 1
+fi
