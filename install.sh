2ba103b0137be55287c49f235bb066bdd7d53ab0
Fabrizio Gortani <fabrizio.gortani@beliven.com>
2024-12-29 20:23:10 +0100

    chore: improve install script

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-trap _cleanup EXIT
-
-_cleanup() {
-	rm -rf /tmp/archive.zip
-	rm -rf /tmp/archive
-}
-
 key_to_link="zipball_url"
 vendor="beliven-it"
 repository_name="laravel-sync"
 repository="$vendor/$repository_name"
+archive_name="archive"
+
+archive_path_folder="/tmp/$archive_name"
+archive_path_zip="$archive_path_folder.zip"
+
+trap _cleanup EXIT
+
+_cleanup() {
+	rm -rf $archive_path_zip
+	rm -rf $archive_path_folder
+}
 
 latest_release=$(curl -s https://api.github.com/repos/$repository/releases/latest | grep "$key_to_link" | cut -d '"' -f 4)
 
@@ -18,9 +22,9 @@ if [ -n "$latest_release" ]; then
 	echo "Latest release: $latest_release"
 	echo "Download the latest release of $repository ..."
 
-	curl -sL "$latest_release" -o /tmp/archive.zip
-	unzip -q -j /tmp/archive.zip -d /tmp/archive
-	cp -r /tmp/archive/$repository_name .
+	curl -sL "$latest_release" -o $archive_path_zip
+	unzip -q -j $archive_path_zip -d $archive_path_folder
+	cp -r $archive_path_folder/$repository_name .
 	chmod +x $repository_name
 	echo "Done!"
 else
